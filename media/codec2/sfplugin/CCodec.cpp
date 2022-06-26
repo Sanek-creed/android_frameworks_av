littleGnAl/mirror_platform_frameworks_av

Implement PUSH_BLANK_BUFFERS_ON_STOP in Codec2
PUSH_BLANK_BUFFERS_ON_STOP is a MediaCodec feature that instructs a
codec to display a blank frame at end of playback on codecs configured
with an output surface.

The feature has existed for a while and been implemented for OMX-based
codecs, but it was missing from the newer Codec2-based codecs.

Test: WIP
Bug: 210587592
Change-Id: I3ec3117e991224134cb6cb6b6a601e063e96be26

media/codec2/sfplugin/CCodec.cpp
@@ -872,6 +872,11 @@ void CCodec::configure(const sp<AMessage> &msg) {
                         }
                         config->mTunneled = true;
                     }
+
+                    int32_t pushBlankBuffersOnStop = 0;
+                    if (msg->findInt32(KEY_PUSH_BLANK_BUFFERS_ON_STOP, &pushBlankBuffersOnStop)) {
+                        config->mPushBlankBuffersOnStop = pushBlankBuffersOnStop == 1;
+                    }
                 }
             }
             setSurface(surface);
@@ -1918,6 +1923,13 @@ void CCodec::initiateRelease(bool sendCallback /* = true */) {
             config->mInputSurface->disconnect();
             config->mInputSurface = nullptr;
             config->mInputSurfaceDataspace = HAL_DATASPACE_UNKNOWN;
+        }
+    }
+    {
+        Mutexed<std::unique_ptr<Config>>::Locked configLocked(mConfig);
+        const std::unique_ptr<Config> &config = *configLocked;
+        if (config->mPushBlankBuffersOnStop) {
+            mChannel->pushBlankBuffer();
         }
     }
 

Sanek-creed/android_frameworks_av

Test: atest DecoderPushBlankBuffersOnStopTest#testPushBlankBuffersOnStopAvc
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

media/codec2/sfplugin/CCodec.cpp
@@ -872,6 +872,11 @@ void CCodec::configure(const sp<AMessage> &msg) {
                         }
                         config->mTunneled = true;
                     }
+
+                    int32_t pushBlankBuffersOnStop = 0;
+                    if (msg->findInt32(KEY_PUSH_BLANK_BUFFERS_ON_STOP, &pushBlankBuffersOnStop)) {
+                        config->mPushBlankBuffersOnStop = pushBlankBuffersOnStop == 1;
+                    }
                 }
             }
             setSurface(surface);
@@ -1814,7 +1819,13 @@ void CCodec::initiateStop() {
         }
         state->set(STOPPING);
     }
-
+    {
+        Mutexed<std::unique_ptr<Config>>::Locked configLocked(mConfig);
+        const std::unique_ptr<Config> &config = *configLocked;
+        if (config->mPushBlankBuffersOnStop) {
+            mChannel->pushBlankBufferToOutputSurface();
+        }
+    }
     mChannel->reset();
     (new AMessage(kWhatStop, this))->post();
 }
@@ -1900,6 +1911,13 @@ void CCodec::initiateRelease(bool sendCallback /* = true */) {
             config->mInputSurface->disconnect();
             config->mInputSurface = nullptr;
             config->mInputSurfaceDataspace = HAL_DATASPACE_UNKNOWN;
+        }
+    }
+    {
+        Mutexed<std::unique_ptr<Config>>::Locked configLocked(mConfig);
+        const std::unique_ptr<Config> &config = *configLocked;
+        if (config->mPushBlankBuffersOnStop) {
+            mChannel->pushBlankBufferToOutputSurface();
         }
     }
 